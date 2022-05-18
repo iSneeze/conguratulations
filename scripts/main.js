9912d07087d7f174fd000068fce5419afbcd6a69
Florian Semaniuk <florian.semaniuk@gmail.com>
2022-05-18 19:29:26 +0200

    first demo

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+
+/* Set the width of the side navigation to 250px */
+function sharkGoesA() {
+    console.log("yeet");
+    var audio = new Audio('assets/audio/gawr-gura-a.mp3');
+    audio.play();
+}
