3c7c9a55b9ef70e36ecd6b98f4490c3ec43c00e7
iSneeze <yeetallthespam@gmail.com>
2022-05-18 19:52:31 +0200

    changed sound resource links to github, wtf

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,6 @@
 
 /* Set the width of the side navigation to 250px */
 function sharkGoesA() {
-    console.log("yeet");
-    var audio = new Audio('assets/audio/gawr-gura-a.mp3');
+    var audio = new Audio('https://isneeze.github.io/conguratulations/assets/audio/gawr-gura-a.mp3');
     audio.play();
 }
