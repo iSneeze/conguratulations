c078c5881e81dec340953fa427b3bb8b77979d31
iSneeze <yeetallthespam@gmail.com>
2022-06-19 02:47:47 +0200

    added avril, added modal, edited front page, font

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
 
+function initModal() {
+    // Get the modal
+    var modal = document.getElementById("modal");
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementsByClassName("modalImg");
+    var modalImg = document.getElementById("imgModal");
+    var captionText = document.getElementById("caption");
+
+    for (let i = 0; i < img.length; i++) {
+        const element = img[i];
+        element.onclick = function () {
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+    }
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+}
+
 //canvasConfetti = createCanvas("confetti");
 
 const countdown = document.getElementById("countdown");
@@ -19,6 +46,7 @@ let secretCounter = 5;
 function showSite() {
     document.getElementById("countdown").style.display = "none";
     document.getElementById("visibilityContainer").style.display = "block";
+    initModal();
 }
 
 function secretaccess() {
@@ -52,3 +80,4 @@ function timer() {
     }
 }
 timer();
+
