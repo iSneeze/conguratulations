f63c64acc17c1c2ad4be49adfcd3014b0c86bcf7
iSneeze <yeetallthespam@gmail.com>
2022-06-20 21:47:29 +0200

    removed countdown, fixed random stuffs

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ function initModal() {
     var modalImg = document.getElementById("imgModal");
     var captionText = document.getElementById("caption");
 
+    if (img.length == 0) {
+        setTimeout(() => {
+            initModal();
+        }, 1000);
+    }
+
     for (let i = 0; i < img.length; i++) {
         const element = img[i];
         element.onclick = function () {
@@ -26,58 +32,6 @@ function initModal() {
     }
 }
 
-//canvasConfetti = createCanvas("confetti");
+initModal();
 
-const countdown = document.getElementById("countdown");
-
-const birthdayDate = new Date(Date.UTC(2022, 5, 20, 4));
-
-const second = 1000;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
-
-
-
-let timeSpan = birthdayDate - new Date();
-
-let secretCounter = 5;
-
-function showSite() {
-    document.getElementById("countdown").style.display = "none";
-    document.getElementById("visibilityContainer").style.display = "block";
-    initModal();
-}
-
-function secretaccess() {
-    secretCounter--;
-    if (secretCounter <= 0) {
-        showSite();
-    }
-}
-
-function timer() {
-    let now = new Date();
-    let diff = birthdayDate - now;
-
-    if (diff > 0) {
-        let days = Math.floor(diff / day);
-        let hours = Math.floor((diff % day) / hour);
-        let minutes = Math.floor((diff % hour) / minute);
-        let seconds = Math.floor((diff % minute) / second);
-
-        document.getElementById("d").innerText = days;
-        document.getElementById("h").innerText = hours;
-        document.getElementById("m").innerText = minutes;
-        document.getElementById("s").innerText = seconds;
-
-        setTimeout(() => {
-            timer();
-        }, 1000);
-    }
-    else {
-        showSite();
-    }
-}
-timer();
 
